976ed27d5083b982296f62708c09061d41bd9cb2
Zohaib Rauf <zabirauf@gmail.com>
2016-10-16 14:39:00 -0700

    Let there be light

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: mono build/SuaveBootstrapFlynn.exe
---
+++ SuaveBootstrapFlynn/SuaveBootstrapFlynn.fs
@@ -0,0 +1,33 @@
+module SuaveBootstrapFlynn
+
+open Suave
+open Suave.Successful
+open Suave.Web
+open Suave.Operators
+open Suave.Filters
+
+open System
+open System.Net
+open System.Threading.Tasks
+
+let helloWorld _ = 
+    printfn "Saying hello world from F# and flynn %O" DateTime.UtcNow
+    OK (sprintf "<html><body><h1>Hello World - %O</h1></body></html>" DateTime.UtcNow)
+
+let app = 
+        GET >=> path "/" >=> request helloWorld
+
+let config = 
+    let port = System.Environment.GetEnvironmentVariable("PORT")
+    let ip127  = IPAddress.Parse("127.0.0.1")
+    let ipZero = IPAddress.Parse("0.0.0.0")
+
+    { defaultConfig with 
+        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Verbose
+        bindings=[ (if port = null then HttpBinding.mk HTTP ip127 (uint16 8080)
+                    else HttpBinding.mk HTTP ipZero (uint16 port)) ] }
+                   
+[<EntryPoint>]
+let main argv =
+    startWebServer config app
+    0 // return an integer exit code
